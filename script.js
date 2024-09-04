4ff5e18d9702244c976ad8b257dd7ddd4f611ae8
prachin12 <magarprachin12@gmail.com>
2024-09-05 09:31:12 +1000

    working properly

--- script.js
+++ script.js
@@ -1,126 +1,117 @@
+let board = [];
+let win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+           [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+           [0, 4, 8], [2, 4, 6]];
+let trigger = 0;
+let count = 0;
 
-
-    //creating a 2d array for the gameboard
-    let board=[];
-
-    //win scenarios
-    let win=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-
-
-    let trigger=0;
-    let count;
-
-    //game inizialization
-    const init = ()=>{
-        for(let i=0;i<9;i++)
-        {
-            board[i]=0;
-        }
-        trigger=0;
-        count=0;
-        resetBoard();
+// Game initialization
+const init = () => {
+    for (let i = 0; i < 9; i++) {
+        board[i] = 0;
     }
-     // reset the board's DOM elements and remove any existing event listeners
-     const resetBoard = () => {
-        let columns = document.querySelectorAll('.column');
-        columns.forEach(column => {
-            column.removeEventListener('click', player); // Remove event listener
-            column.innerText = ""; // Clear text
-        });
-        dom(); // Re-add event listeners
-    }
+    dom();
+}
 
+// Selecting from DOM
+const dom = () => {
+    let columns = document.querySelectorAll('.column');
+    addListener(columns);
+}
 
-    //selecting from dom
-    const dom=()=>{
-        let column=document.querySelectorAll('.column');
-        addlistener(column);
-    }
+const addListener = (columns) => {
+    let id = 0;
+    columns.forEach(column => {
+        column.id = id++;
+        column.innerText = "";
 
-    const addlistener=(column)=>{
-      let id=0;
-      column.forEach(column => {  
-        //adding id
-        column.id=id;
-        id++;
-        column.innerText="";
+        // Remove any existing event listeners
+        column.removeEventListener('click', handleClick);
 
-        //adding event listener to every column
-        column.addEventListener('click',()=>{
-            player(column);
-        });
+        // Attach the event listener
+        column.addEventListener('click', handleClick);
+    });
+}
 
-      });
-    }
+const handleClick = (event) => {
+    player(event.target);
+    console.log(event);
+}
 
-    //player logics
-    const player=(column)=>{
-        let index=column.id;
-        if(board[index]==0)
-        {   
+// Player logic
+const player = (column) => {
+    let index = column.id;
+
+    if (board[index] == 0) {
         count++;
-            if(trigger==0)
-                {
-                    board[index]=1;
-                    trigger=1;
-                    column.innerText='X';
-                }
-            else
-            {
-                board[index]=2;
-                trigger=0;
-                column.innerText='O'
-            }
-        }
-        //check winner
-        if(count>4){
-            let re=wincheck();
-            if(re==1){
-                console.log('x won')
-                playagain();
-            }
-            else if(re==2){
-                console.log('O won');
-                playagain();
-            }
-            else if(count===9){
-                console.log('draw');
-                playagain();
-               
-            }
+        if (trigger == 0) {
+            board[index] = 1;
+            trigger = 1;
+            column.innerText = 'X';
+        } else {
+            board[index] = 2;
+            trigger = 0;
+            column.innerText = 'O';
         }
     }
 
-    const wincheck=()=>{
-        let xcount,ocount;
-        for(let i=0;i<=7;i++){
-            xcount=0;
-            ocount=0;
-            for(let j=0;j<=2;j++)
-            {
-                if(board[win[i][j]]==1){
-                    xcount+=1;
-                }
-                else if(board[win[i][j]]==2){
-                    ocount+=1;
-                }
-            }
-            if(xcount==3){
-                console.log("x won");
-                return 1;
-            }
-            else if(ocount==3){
-                console.log("o won");
-                return 2;
+    // Check for a winner
+    if (count > 4) {
+        let re = winCheck();
+        if (re == 1) {
+            console.log('X won');
+            reset();
+        } else if (re == 2) {
+            console.log('O won');
+            reset();
+        } else if (count === 9) {
+            console.log('Draw');
+            reset();
+        }
+    }
+}
+
+const winCheck = () => {
+    let xCount, oCount;
+    for (let i = 0; i <= 7; i++) {
+        xCount = 0;
+        oCount = 0;
+        for (let j = 0; j <= 2; j++) {
+            if (board[win[i][j]] == 1) {
+                xCount++;
+            } else if (board[win[i][j]] == 2) {
+                oCount++;
             }
         }
-        
+        if (xCount == 3) return 1; // X won
+        if (oCount == 3) return 2; // O won
     }
-    
-    const playagain=()=>{
-        
+    return 0; // No winner yet
+}
+
+const reset = () => {
+    let columns = document.querySelectorAll('.column');
+    columns.forEach(column => {
+        column.innerText = "";
+        // Remove the event listener
+        column.removeEventListener('click', handleClick);
+    });
+
     // Reset game state
-    init();
+    for (let i = 0; i < 9; i++) {
+        board[i] = 0;
     }
-    init();
+    console.log(board);
+    trigger = 0;
+    count = 0;
 
+    // Reinitialize the game
+    playAgain();
+}
+
+const playAgain = () => {
+    init();
+}
+
+// Start the game
+init();
